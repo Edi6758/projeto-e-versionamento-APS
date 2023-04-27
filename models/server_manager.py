3566041f970bcfbf2a5622552c803c457f09153c
SalMolhado <gabrielavila01@gmail.com>
2023-04-27 16:38:17 -0300

    correção em conectar-se a servidor e encontrar partida

--- models/server_manager.py
+++ models/server_manager.py
@@ -8,21 +8,24 @@ class ServerManager:
         message = None
         player_name = self.player_interface.window_manager.input_popup(title='Player Identification',
                                                                        message='Como você se chama?')
-        try:
+        disconnected = True
+        while disconnected:
             message = self.player_interface.dog_server_interface.initialize(player_name, self.player_interface)
             print(message)
-        except Exception as error_message:
-            self.player_interface.window_manager.popup(message=error_message)
-        finally:
             if message != 'Conectado a Dog Server':
-                self.player_interface.window_manager.popup(message=message)
+                break
+        self.player_interface.window_manager.popup(message=message)
 
     def start_match(self):
-        start_status = self.player_interface.dog_server_interface.start_match(2)
-        message = start_status.get_message()
-        print(message)
-        if message == 'Partida iniciada':
-            self.player_interface.window_manager.swap_to_hero_creator()
+        opponent_unavailable = True
+        while opponent_unavailable:
+            start_status = self.player_interface.dog_server_interface.start_match(2)
+            message = start_status.get_message()
+            print(message)
+            if message == 'Partida iniciada':
+                break
+            print('tentando novamente')
+        self.player_interface.window_manager.swap_to_hero_creator()
 
     @property
     def player_interface(self):
