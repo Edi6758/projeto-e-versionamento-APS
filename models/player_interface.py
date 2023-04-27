ac5f572707e1aab22d6ca868a1d8667cd2a74ffe
SalMolhado <gabrielavila01@gmail.com>
2023-04-27 18:11:10 -0300

    ServerManager agora tem todas as responsabilidades a respeito de conectividade

--- models/player_interface.py
+++ models/player_interface.py
@@ -1,32 +1,18 @@
 from screens.window_manager import WindowManager
-from dog.dog_interface import DogPlayerInterface
-from dog.dog_actor import DogActor
 from models.server_manager import ServerManager
 from models.battle_manager import BattleManager
 
 
-class PlayerInterface(DogPlayerInterface):
+class PlayerInterface:
     def __init__(self):
-        super().__init__()
-        self.__dog_server_interface = DogActor()
         self.__server_manager = ServerManager(player_interface=self)
         self.__window_manager = WindowManager(player_interface=self)
         self.__battle_manager = BattleManager(player_interface=self)
         self.server_manager.connect_to_server()
 
-    def receive_start(self, start_status):
-        message = start_status.get_message()
-        print(message)
-        if start_status.get_code == 2:
-            self.window_manager.swap_to_hero_creator()
-
     @property
     def window_manager(self):
         return self.__window_manager
-
-    @property
-    def dog_server_interface(self):
-        return self.__dog_server_interface
 
     @property
     def server_manager(self):
--- models/server_manager.py
+++ models/server_manager.py
@@ -1,34 +1,57 @@
-from time import sleep
+from dog.dog_interface import DogPlayerInterface
+from dog.dog_actor import DogActor
 
 
-class ServerManager:
+class ServerManager(DogPlayerInterface):
     def __init__(self, player_interface):
+        super().__init__()
+        self.__dog_server_interface = DogActor()
         self.__player_interface = player_interface
+        self.__player_name = ''
 
     def connect_to_server(self):
-        message = None
-        player_name = self.player_interface.window_manager.input_popup(title='Player Identification',
-                                                                       message='Como você se chama?')
-        disconnected = True
-        while disconnected:
-            message = self.player_interface.dog_server_interface.initialize(player_name, self.player_interface)
-            print(message)
-            if message == 'Conectado a Dog Server':
-                break
-        self.player_interface.window_manager.popup(message=message)
+        self.player_name = self.player_interface.window_manager.input_popup(title='Player Identification',
+                                                                            message='Como você se chama?')
+        self.trying_connection()
+
+    def trying_connection(self):
+        message = self.dog_server_interface.initialize(self.player_name, self)
+        if message != 'Conectado a Dog Server':
+            print('tentando novamente em 3 segundos')
+            self.player_interface.window_manager.window.after(3000, self.trying_connection())
+        else:
+            return message
 
     def start_match(self):
-        start_status = self.player_interface.dog_server_interface.start_match(2)
+        start_status = self.dog_server_interface.start_match(2)
         message = start_status.get_message()
         print(message)
-        if start_status.get_code == 2:
+        code = start_status.get_code()
+        print(code)
+        if code == '2':
             self.player_interface.window_manager.swap_to_hero_creator()
-            return
-        else:
+        elif code == '1':
             print('tentando novamente em 3 segundos')
             self.player_interface.window_manager.window.after(3000, self.start_match)
 
+    def receive_start(self, start_status):
+        message = start_status.get_message()
+        print(message)
+        if start_status.get_code == '2':
+            self.player_interface.window_manager.swap_to_hero_creator()
 
     @property
     def player_interface(self):
         return self.__player_interface
+
+    @property
+    def dog_server_interface(self):
+        return self.__dog_server_interface
+
+    @property
+    def player_name(self):
+        return self.__player_interface
+
+    @player_name.setter
+    def player_name(self, player_name):
+        self.__player_name = player_name
